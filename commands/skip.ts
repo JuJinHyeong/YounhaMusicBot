15e1de1750abced1fecebecabe064e47724af62a
unknown <jujh97@hanmail.net>
2022-11-07 00:01:36 +0900

    add skip

--- commands/skip.ts
+++ commands/skip.ts
@@ -6,14 +6,14 @@ class SkipCommand extends SlashCommand {
         super(creator, {
             name: 'skip',
             description: 'Skip to the current song',
-
+            
             guildIDs: process.env.DISCORD_GUILD_ID ? [ process.env.DISCORD_GUILD_ID ] : undefined
         });
     }
 
     async run(ctx: CommandContext) {
         await ctx.defer();
-        const queue = manager.player.getQueue(ctx.guildID);
+        const queue = manager.player.getQueue(ctx.guildID || '');
         if (!queue || !queue.playing) return void ctx.sendFollowUp({ content: '❌ | No music is being played!' });
         const currentTrack = queue.current;
         const success = queue.skip();
